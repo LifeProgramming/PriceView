0f2b8e730fd838b5b93007b97f77f23371d542ae
Ali <parsa201909@gmail.com>
2023-08-23 15:39:07 +0330

    version1

--- BitCoin.py
+++ BitCoin.py
@@ -6,6 +6,23 @@ def BitCoinPrice():
     url=requests.get('https://coinmarketcap.com/currencies/bitcoin/')
     soup=BeautifulSoup(url.text,'lxml')
     PricePart=soup.find('span',{'class':'sc-16891c57-0 dxubiK base-text'})
+    VolumePart=soup.find_all('dd',{"class":"sc-16891c57-0 fRWxhs base-text"})[1].text
+    CirculatingSupply=soup.find_all('dd',{'class':'sc-16891c57-0 fRWxhs base-text'})[3].text
+    MarketCapitalization=soup.find('dd',{'class':'sc-16891c57-0 fRWxhs base-text'}).text
 
-    return PricePart.text
+    # Find the index of the first `$` character.
+    index = VolumePart.find("$")
+    index1=MarketCapitalization.find('$')
+
+    # Return the substring starting from the index of the `$` character.
+    Volume=VolumePart[index :]
+    MarketCap=MarketCapitalization[index1:]
+
+    return Volume, PricePart.text,CirculatingSupply,MarketCap
+
+
+
+
+
+
 
--- ViewBoard.py
+++ ViewBoard.py
@@ -8,10 +8,13 @@ print('What crypto currency do you want to view? ')
 Crypto=input('Your crypto : ')
 try:
     if Crypto=='Bitcoin':
-        print(BitCoinPrice())
+        getBitcoin=BitCoinPrice()
+        print(f' Price : {getBitcoin[1]} \n Trading Volume(24h) : {getBitcoin[0]} \n Circulating Supply : {getBitcoin[2]} \n Market Capitalization : {getBitcoin[3]} ')
     elif Crypto=='Ethereum':
-        print(EthereumPrice())
+        getEthereum=EthereumPrice()
+        print(f' Price : {getEthereum[1]} \n Trading Volume(24h) : {getEthereum[0]} \n Circulating Supply : {getEthereum[2]}  \n Market Capitalization : {EthereumPrice()[3]}')
     elif Crypto=='Cardano':
-        print(CardanoPrice())
+        getCardano=CardanoPrice()
+        print(f' Price : {getCardano[1]} \n Trading Volume(24h) : {getCardano[0]}\n Circulating Supply : {getCardano[2]}  \n Market Capitalization : {getCardano[3]}')
 except:
     print('Sorry! Something went wrong!!! Pease try again later.')
